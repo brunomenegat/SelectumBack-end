91dd03c4d6cae6136b732312fd24381275b83da0
kakaroto12 <b.menegatt@hotmail.com>
2020-05-15 19:34:07 -0300

    foi

--- app.js
+++ app.js
@@ -3,30 +3,14 @@ const bodyParser = require("body-parser"); //Facilita a transmissão do back e f
 const app = express(); //criando app
 const mongoose = require("mongoose"); // conexão banco de dados
 const clientRouter = require("./routes/clientcontroller");
-//const MongoClient = require('mongodb').MongoClient;
-//const uri = "mongodb+srv://bno:bnobno@cluster0-vmlmz.mongodb.net/test?retryWrites=true&w=majority";
-//const client = new MongoClient(uri, { useNewUrlParser: true });
 
 //Conectar com database
 mongoose
   .connect(
     //"mongodb+srv://bno:bnobno@cluster0-vmlmz.mongodb.net/test?retryWrites=true&w=majority",
-    
     "mongodb://localhost:27017",
     { useNewUrlParser: true }
   ) //criadnoconexão e define mongoclient
   .then(() => console.log("DB conectado!!"))
   .catch(err => console.log(`Algum erro ocorreu: ${err}`)); // Função que pega os erro.
-
-
-//controle fluxo de dados
-//app.use(express.json());
-app.use(bodyParser.json()); //utilizar json - entenda requisicao
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Rotas
-require("./routes/authcontroller")(app);  // @@Auth - Autenticação
-
-app.post("/client", clientRouter);  // @@Client - 
-app.use("", express.static("front"));  // @@HomePage - Páginas
-app.listen(5000, () => console.log("Server started."));  // Ligar o servidor na porta especificada
+    
