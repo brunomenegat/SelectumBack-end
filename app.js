217375926281a55c548dcfb3e9f7e748af3e4de5
kakaroto12 <b.menegatt@hotmail.com>
2020-05-16 14:06:42 -0300

    appjs e indexjs

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require("express");
-const mongoose = require("mongoose"); // conexão banco de dados
 
 
-//Conectar com database
-mongoose
-  .connect(
-    //"mongodb+srv://bno:bnobno@cluster0-vmlmz.mongodb.net/test?retryWrites=true&w=majority",
-    "mongodb://localhost:27017",
-    { useNewUrlParser: true }
-  ) //criadnoconexão e define mongoclient
-  .then(() => console.log("DB conectado!!"))
-  .catch(err => console.log(`Algum erro ocorreu: ${err}`)); // Função que pega os erro.
+const app = express();
+
+
+app.get('/users', (request, response) => {
+ 
+  return Response.json({
+    TipoCliente: 'Comercio',
+    Cliente: 'Ryori'
+
+  });
+});
+
+app.listen(3333);
