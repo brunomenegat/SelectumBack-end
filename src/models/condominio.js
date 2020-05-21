6c7a6819ebcbf81e8704c95e60be7742336d3df6
V-Mihael <vmharissis@gmail.com>
2020-05-21 13:30:46 -0300

    Adicionado modelo inicial de condominio

---
+++ src/models/condominio.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+var CondominioSchema = new mongoose.Schema({
+    nome: String,
+    fone: String,
+    endereço: String,
+    apartamentos: [{total: Number, contratados: Number, naoContratados: Number}],
+    CriadoEm: {type: Date, default: Date.now}
+});
+
+var Condominio = mongoose.model('Condominio', CondominioSchema);
+
+module.exports = Client;
